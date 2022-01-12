b53629eb8973839e34e8e735d50ebbe6f73fdb70
p22mbonilla <p22mbonilla@fairfieldprep.org>
2022-01-12 10:20:22 -0500

    addfile

---
+++ hw9-2-1.py
@@ -0,0 +1,12 @@
+# Author MB 01/12/2022
+
+def intials():
+    for row in rows:
+        for a in row:
+
+last_initials = ["B.", "D.", "H.", "E.", "G.", "G.", "H.", "R.", "M.", "L.", "I.", "I.", "N.", "N.", "O.", "P.", "P.", "X.", "T.", "S.", "S.", "P."]
+
+rows = [["Mateo", "Jason", "Jordan", "Mohamed", "Michael", "Charlie", "Declan"], ["Sean", "Luis", "Ryan", "James", "Jack"], ["Aiden", "Ian", "Colin", "Tim", "Cam"], ["Larry", "Spencer", "Chris", "Ryan", "Nolan"]]
+
+print(rows)
+        
