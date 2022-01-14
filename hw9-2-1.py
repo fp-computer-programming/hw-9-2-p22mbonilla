c24233dab88ead1fd866ccf9ffefe91af29f462e
p22mbonilla <p22mbonilla@fairfieldprep.org>
2022-01-14 10:19:02 -0500

    add file

--- hw9-2-1.py
+++ hw9-2-1.py
@@ -1,12 +1,15 @@
 # Author MB 01/12/2022
 
-def initials():
+def initials(rows):
     for row in rows:
         for a in row:
+            if a == 0:
+                row += 1
+                
 
-last_initials = ["B.", "D.", "H.", "E.", "G.", "G.", "H.", "R.", "M.", "L.", "I.", "I.", "N.", "N.", "O.", "P.", "P.", "X.", "T.", "S.", "S.", "P."]
+last_intitial = ["B.", "D.", "H.", "E.", "G.", "G.", "H.", "R.", "M.", "L.", "I.", "I.", "N.", "N.", "O.", "P.", "P.", "X.", "T.", "S.", "S.", "P."]
 
 rows = [["Mateo", "Jason", "Jordan", "Mohamed", "Michael", "Charlie", "Declan"], ["Sean", "Luis", "Ryan", "James", "Jack"], ["Aiden", "Ian", "Colin", "Tim", "Cam"], ["Larry", "Spencer", "Chris", "Ryan", "Nolan"]]
 
-print(rows)
+print(initials(rows))
         
